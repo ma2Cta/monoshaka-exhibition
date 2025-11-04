c73e6cc1fb86389acbbf0843537372e25276ffea
ma2Cta <mtstdisk@gmail.com>
2025-11-05 01:30:55 +0900

    フィードバック付き録音画面

---
+++ src/app/record/feedback/page.tsx
@@ -0,0 +1,11 @@
+import AudioRecorderWithFeedback from '@/components/recorder/AudioRecorderWithFeedback';
+import Header from '@/components/layout/Header';
+
+export default function RecordFeedbackPage() {
+  return (
+    <>
+      <Header />
+      <AudioRecorderWithFeedback />
+    </>
+  );
+}
